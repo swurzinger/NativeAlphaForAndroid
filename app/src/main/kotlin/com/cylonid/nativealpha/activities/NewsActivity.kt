11918ce74e936ebb2a5d5b4c3031d930336158fb
Stefan Wurzinger <swurzinger@gmx.at>
2023-04-16 23:22:02 +0200

    update framework, libraries

--- app/src/main/kotlin/com/cylonid/nativealpha/activities/NewsActivity.kt
+++ app/src/main/kotlin/com/cylonid/nativealpha/activities/NewsActivity.kt
@@ -3,7 +3,6 @@ package com.cylonid.nativealpha.activities;
 import android.content.Intent
 import android.graphics.drawable.Drawable
 import android.os.Bundle
-import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewTreeObserver
@@ -12,26 +11,26 @@ import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Toast
-import androidx.annotation.ColorInt
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.cylonid.nativealpha.BuildConfig
 import com.cylonid.nativealpha.R
+import com.cylonid.nativealpha.databinding.NewsActivityBinding
 import com.cylonid.nativealpha.model.DataManager
 import com.cylonid.nativealpha.util.LocaleUtils
 import com.cylonid.nativealpha.util.Utility
-import kotlinx.android.synthetic.main.news_activity.*
-import kotlin.properties.Delegates
 
 
 class NewsActivity : AppCompatActivity(), View.OnTouchListener, ViewTreeObserver.OnScrollChangedListener {
 
     var btnDefaultBackgroundColor: Drawable? = null
     var btnDefaultTextColor: Int = android.R.color.black
+    lateinit var binding: NewsActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.news_activity)
+        binding = NewsActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         initializeUI()
         setButtonState()
@@ -41,23 +40,23 @@ class NewsActivity : AppCompatActivity(), View.OnTouchListener, ViewTreeObserver
     override fun onBackPressed() {}
 
     private fun disableAcceptButton() {
-        btnNewsConfirm.isActivated = false
-        btnDefaultTextColor = btnNewsConfirm.currentTextColor
-        btnDefaultBackgroundColor = btnNewsConfirm.background
+        binding.btnNewsConfirm.isActivated = false
+        btnDefaultTextColor = binding.btnNewsConfirm.currentTextColor
+        btnDefaultBackgroundColor = binding.btnNewsConfirm.background
 
-        btnNewsConfirm.setBackgroundColor(
+        binding.btnNewsConfirm.setBackgroundColor(
             ContextCompat.getColor(
                 baseContext,
                 R.color.disabled_background_color
             )
         )
-        btnNewsConfirm.setTextColor(
+        binding.btnNewsConfirm.setTextColor(
             ContextCompat.getColor(
                 baseContext,
                 R.color.disabled_text_color
             )
         )
-        btnNewsConfirm.setOnClickListener {
+        binding.btnNewsConfirm.setOnClickListener {
             Utility.showToast(
                 this,
                 getString(R.string.scroll_to_bottom),
@@ -67,30 +66,30 @@ class NewsActivity : AppCompatActivity(), View.OnTouchListener, ViewTreeObserver
     }
 
     private fun enableAcceptButton() {
-        btnNewsConfirm.isActivated = true
-        btnNewsConfirm.setTextColor(btnDefaultTextColor)
-        btnNewsConfirm.background = btnDefaultBackgroundColor
+        binding.btnNewsConfirm.isActivated = true
+        binding.btnNewsConfirm.setTextColor(btnDefaultTextColor)
+        binding.btnNewsConfirm.background = btnDefaultBackgroundColor
 
-        btnNewsConfirm.setOnClickListener { confirm() }
+        binding.btnNewsConfirm.setOnClickListener { confirm() }
     }
 
     private fun initializeUI() {
         setText()
-        btnNewsConfirm.setOnClickListener {
+        binding.btnNewsConfirm.setOnClickListener {
             confirm()
         }
     }
 
     private fun setButtonState() {
-        val vto: ViewTreeObserver = news_scrollchild.viewTreeObserver
+        val vto: ViewTreeObserver = binding.newsScrollchild.viewTreeObserver
         vto.addOnGlobalLayoutListener(object : OnGlobalLayoutListener {
             override fun onGlobalLayout() {
-                val height: Int = news_scrollchild.measuredHeight
+                val height: Int = binding.newsScrollchild.measuredHeight
                 if (height > 0) {
-                    news_scrollchild.viewTreeObserver.removeOnGlobalLayoutListener(this)
-                    if (news_scrollview.canScrollVertically(1) || news_scrollview.canScrollVertically(-1)) {
-                        news_scrollview.setOnTouchListener(this@NewsActivity)
-                        news_scrollview.viewTreeObserver.addOnScrollChangedListener(this@NewsActivity)
+                    binding.newsScrollchild.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                    if (binding.newsScrollview.canScrollVertically(1) || binding.newsScrollview.canScrollVertically(-1)) {
+                        binding.newsScrollview.setOnTouchListener(this@NewsActivity)
+                        binding.newsScrollview.viewTreeObserver.addOnScrollChangedListener(this@NewsActivity)
                         disableAcceptButton()
                     }
                 }
@@ -101,11 +100,11 @@ class NewsActivity : AppCompatActivity(), View.OnTouchListener, ViewTreeObserver
     private fun setText() {
         val fileId = intent.extras?.getString("text") ?: "latestUpdate"
 
-        news_content.loadUrl("file:///android_asset/news/" + fileId + "_" + LocaleUtils.fileEnding +".html")
+        binding.newsContent.loadUrl("file:///android_asset/news/" + fileId + "_" + LocaleUtils.fileEnding +".html")
         if(DataManager.getInstance().eulaData) {
-            btnNewsConfirm.isEnabled = true
-            news_content.settings.javaScriptEnabled = true
-            news_content.webViewClient = NewsWebViewClient()
+            binding.btnNewsConfirm.isEnabled = true
+            binding.newsContent.settings.javaScriptEnabled = true
+            binding.newsContent.webViewClient = NewsWebViewClient()
         }
     }
 
@@ -120,8 +119,8 @@ class NewsActivity : AppCompatActivity(), View.OnTouchListener, ViewTreeObserver
     }
 
     override fun onScrollChanged() {
-        val view = news_scrollview.getChildAt(news_scrollview.childCount - 1)
-        val bottomDetector: Int = view.bottom - (news_scrollview.height + news_scrollview.scrollY)
+        val view = binding.newsScrollview.getChildAt(binding.newsScrollview.childCount - 1)
+        val bottomDetector: Int = view.bottom - (binding.newsScrollview.height + binding.newsScrollview.scrollY)
 
         if (bottomDetector < 30) {
            enableAcceptButton()
